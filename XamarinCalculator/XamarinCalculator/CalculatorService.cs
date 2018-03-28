bd6956d32cda157c02210a07915ec579c75640ef
Natasha R <36881541+tasha-r@users.noreply.github.com>
2018-03-28 21:35:16 +0100

    Implements calculator service and tests to deal with two numbers

---
+++ XamarinCalculator/XamarinCalculator/CalculatorService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinCalculator
+{
+    public class CalculatorService
+    {
+        private double firstNumber;
+
+        private double secondNumber;
+
+        public double FirstNumber { get => firstNumber; set => firstNumber = value; }
+
+        public double SecondNumber { get => secondNumber; set => secondNumber = value; }
+
+        public CalculatorService()
+        {
+        }
+
+        public CalculatorService(double firstNumberInput, double secondNumberInput)
+        {
+            firstNumber = firstNumberInput;
+            secondNumber = secondNumberInput;
+        }
+
+        public double AddNumbers()
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double SubtractNumbers()
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public double MultiplyNumbers()
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public double DivideNumbers()
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
