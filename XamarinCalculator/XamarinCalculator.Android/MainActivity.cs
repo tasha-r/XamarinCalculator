6bc7be3b39a2e66433ba475a5db5f64557370962
Natasha R <36881541+tasha-r@users.noreply.github.com>
2018-03-28 20:03:05 +0100

    Adds StyleCop to repository

--- XamarinCalculator/XamarinCalculator.Android/MainActivity.cs
+++ XamarinCalculator/XamarinCalculator.Android/MainActivity.cs
@@ -1,16 +1,11 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace XamarinCalculator.Droid
 {
     [Activity(Label = "XamarinCalculator", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
@@ -19,9 +14,8 @@ namespace XamarinCalculator.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
     }
 }
-
--- XamarinCalculator/XamarinCalculator/App.xaml.cs
+++ XamarinCalculator/XamarinCalculator/App.xaml.cs
@@ -1,32 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace XamarinCalculator
 {
-	public partial class App : Application
+    public partial class App : Application
 	{
-		public App ()
+		public App()
 		{
 			InitializeComponent();
 
-			MainPage = new XamarinCalculator.MainPage();
+			MainPage = new MainPage();
 		}
 
-		protected override void OnStart ()
+		protected override void OnStart()
 		{
 			// Handle when your app starts
 		}
 
-		protected override void OnSleep ()
+		protected override void OnSleep()
 		{
 			// Handle when your app sleeps
 		}
 
-		protected override void OnResume ()
+		protected override void OnResume()
 		{
 			// Handle when your app resumes
 		}
