4ca5f8d9d6c7c459610e80301cc4a61df9ceb5e6
Natasha R <36881541+tasha-r@users.noreply.github.com>
2018-03-30 17:52:13 +0100

    Refactors calculator service

--- XamarinCalculator.Tests/CalculatorServiceTests.cs
+++ XamarinCalculator.Tests/CalculatorServiceTests.cs
@@ -1,168 +1,42 @@
-﻿using XamarinCalculator;
-using Xunit;
+﻿using Xunit;
 
 namespace XamarinCalculator.Tests
 {
     public class CalculatorServiceTests
     {
         [Fact]
-        public void CalculatorService_WhenCalledWithNumbers_SetsPrivateVariables()
+        public void Calculate_WhenCalledWithAdditionOperator_ReturnsCorrectResult()
         {
-            var calculatorService = new CalculatorService(5, 10);
-            Assert.Equal(5, calculatorService.FirstNumber);
-            Assert.Equal(10, calculatorService.SecondNumber);
+            var result = CalculatorService.Calculate(10, 5.5, '+');
+            Assert.Equal(15.5, result);
         }
 
         [Fact]
-        public void AddNumbers_WhenCalledWithTwoIntegers_ReturnsCorrectValue()
+        public void Calculate_WhenCalledWithSubtractionOperator_ReturnsCorrectResult()
         {
-            var result = AddNumbers(5, 10);
-
-            Assert.Equal(15, result);
+            var result = CalculatorService.Calculate(10, 5.5, '-');
+            Assert.Equal(4.5, result);
         }
 
         [Fact]
-        public void AddNumbers_WhenCalledWithOneIntegerAndOneDouble_ReturnsCorrectValue()
+        public void Calculate_WhenCalledWithMultiplicationOperator_ReturnsCorrectResult()
         {
-            var result = AddNumbers(5, 15.654);
-
-            Assert.Equal(20.654, result);
-        }
-
-        [Fact]
-        public void AddNumbers_WhenCalledWithTwoDoubles_ReturnsCorrectValue()
-        {
-            var result = AddNumbers(5.5, 10.5);
-
-            Assert.Equal(16, result);
-        }
-
-        [Fact]
-        public void AddNumbers_WhenCalledWithNegativeNumber_ReturnsCorrectValue()
-        {
-            var result = AddNumbers(-15, 10.5);
-
-            Assert.Equal(-4.5, result);
-        }
-
-        [Fact]
-        public void SubtractNumbers_WhenCalledWithTwoIntegers_ReturnsCorrectValue()
-        {
-            var result = SubtractNumbers(10, 2);
-
-            Assert.Equal(8, result);
-        }
-
-        [Fact]
-        public void SubtractNumbers_WhenCalledWithOneIntegerAndOneDouble_ReturnsCorrectValue()
-        {
-            var result = SubtractNumbers(10, 2.55);
-
-            Assert.Equal(7.45, result);
-        }
-
-        [Fact]
-        public void SubtractNumbers_WhenCalledWithTwoDoubles_ReturnsCorrectValue()
-        {
-            var result = SubtractNumbers(2.52, 1.52);
-
-            Assert.Equal(1, result);
-        }
-
-        [Fact]
-        public void SubtractNumbers_WhenCalledWithNegativeNumber_ReturnsCorrectValue()
-        {
-            var result = SubtractNumbers(8, -2);
-
-            Assert.Equal(10, result);
-        }
-
-        [Fact]
-        public void MultiplyNumbers_WhenCalledWithTwoIntegers_ReturnsCorrectValue()
-        {
-            var result = MultiplyNumbers(5, 10);
-
+            var result = CalculatorService.Calculate(10, 5, '*');
             Assert.Equal(50, result);
         }
 
         [Fact]
-        public void MultiplyNumbers_WhenCalledWithOneIntegerAndOneDouble_ReturnsCorrectValue()
+        public void Calculate_WhenCalledWithDivisionOperator_ReturnsCorrectResult()
         {
-            var result = MultiplyNumbers(5, 5.2);
-
-            Assert.Equal(26, result);
-        }
-
-        [Fact]
-        public void MultiplyNumbers_WhenCalledWithTwoDoubles_ReturnsCorrectValue()
-        {
-            var result = MultiplyNumbers(5.2, 2.4);
-
-            Assert.Equal(12.48, result);
-        }
-
-        [Fact]
-        public void MultiplyNumbers_WhenCalledWithNegativeNumber_ReturnsCorrectValue()
-        {
-            var result = MultiplyNumbers(-2, 5);
-
-            Assert.Equal(-10, result);
-        }
-
-        [Fact]
-        public void DivideNumbers_WhenCalledWithTwoIntegers_ReturnsCorrectValue()
-        {
-            var result = DivideNumbers(5, 10);
-
-            Assert.Equal(0.5, result);
-        }
-
-        [Fact]
-        public void DivideNumbers_WhenCalledWithOneIntegerAndOneDouble_ReturnsCorrectValue()
-        {
-            var result = DivideNumbers(16, 2.5);
-
-            Assert.Equal(6.4, result);
-        }
-
-        [Fact]
-        public void DivideNumbers_WhenCalledWithTwoDoubles_ReturnsCorrectValue()
-        {
-            var result = DivideNumbers(6.6, 3.3);
-
+            var result = CalculatorService.Calculate(10, 5, '÷');
             Assert.Equal(2, result);
         }
 
         [Fact]
-        public void DivideNumbers_WhenCalledWithNegativeNumber_ReturnsCorrectValue()
+        public void Calculate_WhenCalledWithInvalidOperator_ReturnsCorrectResult()
         {
-            var result = DivideNumbers(-10, 5);
-
-            Assert.Equal(-2, result);
-        }
-
-        private double AddNumbers(double firstNumber, double secondNumber)
-        {
-            var calculatorService = new CalculatorService(firstNumber, secondNumber);
-            return calculatorService.AddNumbers();
-        }
-
-        private double SubtractNumbers(double firstNumber, double secondNumber)
-        {
-            var calculatorService = new CalculatorService(firstNumber, secondNumber);
-            return calculatorService.SubtractNumbers();
-        }
-
-        private double MultiplyNumbers(double firstNumber, double secondNumber)
-        {
-            var calculatorService = new CalculatorService(firstNumber, secondNumber);
-            return calculatorService.MultiplyNumbers();
-        }
-
-        private double DivideNumbers(double firstNumber, double secondNumber)
-        {
-            var calculatorService = new CalculatorService(firstNumber, secondNumber);
-            return calculatorService.DivideNumbers();
+            var result = CalculatorService.Calculate(10, 5, '$');
+            Assert.Equal(0, result);
         }
     }
 }
--- XamarinCalculator/XamarinCalculator/CalculatorService.cs
+++ XamarinCalculator/XamarinCalculator/CalculatorService.cs
@@ -1,47 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace XamarinCalculator
+﻿namespace XamarinCalculator
 {
     public class CalculatorService
     {
-        private double firstNumber;
-
-        private double secondNumber;
-
-        public double FirstNumber { get => firstNumber; set => firstNumber = value; }
-
-        public double SecondNumber { get => secondNumber; set => secondNumber = value; }
-
-        public CalculatorService()
+        public static double Calculate(double firstNumber, double secondNumber, char mathOperator)
         {
-        }
-
-        public CalculatorService(double firstNumberInput, double secondNumberInput)
-        {
-            firstNumber = firstNumberInput;
-            secondNumber = secondNumberInput;
-        }
-
-        public double AddNumbers()
-        {
-            return firstNumber + secondNumber;
-        }
-
-        public double SubtractNumbers()
-        {
-            return firstNumber - secondNumber;
-        }
-
-        public double MultiplyNumbers()
-        {
-            return firstNumber * secondNumber;
-        }
-
-        public double DivideNumbers()
-        {
-            return firstNumber / secondNumber;
+            switch (mathOperator)
+            {
+                case '+':
+                    return firstNumber + secondNumber;
+                case '-':
+                    return firstNumber - secondNumber;
+                case '*':
+                    return firstNumber * secondNumber;
+                case '÷':
+                    return firstNumber / secondNumber;
+                default:
+                    return 0;
+            }
         }
     }
 }
