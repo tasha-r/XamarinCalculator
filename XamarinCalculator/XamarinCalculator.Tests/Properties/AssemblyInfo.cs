db1966e06220d49e97bbfce20cab5dff8ee4d4db
Natasha R <36881541+tasha-r@users.noreply.github.com>
2018-03-30 19:06:08 +0100

    Moves calculator tests project into main calculator folder

--- XamarinCalculator/XamarinCalculator.Tests/Properties/AssemblyInfo.cs
+++ XamarinCalculator/XamarinCalculator.Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("XamarinCalculatorTests")]
+[assembly: AssemblyTitle("XamarinCalculator.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("XamarinCalculatorTests")]
+[assembly: AssemblyProduct("XamarinCalculator.Tests")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("33ace66a-23b9-42ed-814e-8eb3f510f61c")]
+[assembly: Guid("077284d6-157d-4406-bfbe-102438815681")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
