dd30a9a3494a32defc69e99d4728687987412e22
Natasha R <36881541+tasha-r@users.noreply.github.com>
2018-04-02 15:17:07 +0100

    Adds grid and labels to calculator UI

--- XamarinCalculator/XamarinCalculator/MainPage.xaml.cs
+++ XamarinCalculator/XamarinCalculator/MainPage.xaml.cs
@@ -1,12 +1,45 @@
 ﻿using Xamarin.Forms;
+using XamarinCalculator.Helpers;
 
 namespace XamarinCalculator
 {
     public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    {
+        public MainPage()
+        {
+            Padding = SetupPadding();
+            Content = SetupContent();
+        }
+
+        private Thickness SetupPadding()
+        {
+            var topPadding = 0.0;
+
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    topPadding = 20;
+                    break;
+                case Device.Android:
+                    topPadding = 20;
+                    break;
+                default:
+                    topPadding = 0;
+                    break;
+            }
+
+            return new Thickness(20, topPadding, 20, 20);
+        }
+
+        private Grid SetupContent()
+        {
+            var titleLabel = LabelHelper.CreateTitleLabel("Calculator");
+            var outputLabel = LabelHelper.CreateResultLabel("0");
+
+            var grid = GridHelper.CreateGrid(7, 4);
+            GridHelper.AddItemToGrid(grid, titleLabel, 0, 0, 1, 4);
+            GridHelper.AddItemToGrid(grid, outputLabel, 1, 0, 1, 4);
+            return grid;
+        }
+    }
 }
